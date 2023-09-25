960091fbb6e2cd56e5583bdf4f2385ea05e8e809
Fábio Ribeiro <112023955+fabi0ribeir0@users.noreply.github.com>
2023-09-25 14:13:33 -0300

    concluido a idéia

--- Novo_Colaborador/Novo_Colaborador/Form1.Designer.cs
+++ Novo_Colaborador/Novo_Colaborador/Form1.Designer.cs
@@ -35,19 +35,20 @@
             this.txtNome = new System.Windows.Forms.TextBox();
             this.rbPR = new System.Windows.Forms.RadioButton();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtFuncao = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.rbSC = new System.Windows.Forms.RadioButton();
             this.rbRS = new System.Windows.Forms.RadioButton();
+            this.rbSC = new System.Windows.Forms.RadioButton();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.label4 = new System.Windows.Forms.Label();
+            this.chbTela = new System.Windows.Forms.CheckBox();
+            this.chbCel = new System.Windows.Forms.CheckBox();
             this.chbNote = new System.Windows.Forms.CheckBox();
             this.chbChip = new System.Windows.Forms.CheckBox();
-            this.chbCel = new System.Windows.Forms.CheckBox();
-            this.chbTela = new System.Windows.Forms.CheckBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtMail = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
+            this.btnEnviar = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
@@ -57,7 +58,7 @@
             // 
             this.lblTitulo.AutoSize = true;
             this.lblTitulo.Font = new System.Drawing.Font("Eras Bold ITC", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTitulo.Location = new System.Drawing.Point(196, 128);
+            this.lblTitulo.Location = new System.Drawing.Point(82, 161);
             this.lblTitulo.Name = "lblTitulo";
             this.lblTitulo.Size = new System.Drawing.Size(307, 36);
             this.lblTitulo.TabIndex = 0;
@@ -68,9 +69,9 @@
             this.pictureBox1.ErrorImage = null;
             this.pictureBox1.Image = global::Novo_Colaborador.Properties.Resources.logo_medicalway;
             this.pictureBox1.InitialImage = null;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(12, 13);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(658, 111);
+            this.pictureBox1.Size = new System.Drawing.Size(450, 111);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
@@ -79,7 +80,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(9, 213);
+            this.label1.Location = new System.Drawing.Point(12, 206);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(48, 16);
             this.label1.TabIndex = 2;
@@ -87,19 +88,22 @@
             // 
             // txtNome
             // 
-            this.txtNome.Location = new System.Drawing.Point(12, 232);
+            this.txtNome.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNome.Location = new System.Drawing.Point(12, 225);
             this.txtNome.Name = "txtNome";
-            this.txtNome.Size = new System.Drawing.Size(397, 20);
-            this.txtNome.TabIndex = 3;
+            this.txtNome.Size = new System.Drawing.Size(450, 22);
+            this.txtNome.TabIndex = 0;
+            this.txtNome.Leave += new System.EventHandler(this.txtNome_Leave);
             // 
             // rbPR
             // 
             this.rbPR.AutoSize = true;
             this.rbPR.Checked = true;
-            this.rbPR.Location = new System.Drawing.Point(3, 7);
+            this.rbPR.Location = new System.Drawing.Point(6, 8);
             this.rbPR.Name = "rbPR";
             this.rbPR.Size = new System.Drawing.Size(59, 17);
-            this.rbPR.TabIndex = 5;
+            this.rbPR.TabIndex = 0;
             this.rbPR.TabStop = true;
             this.rbPR.Text = "Paraná";
             this.rbPR.UseVisualStyleBackColor = true;
@@ -108,24 +112,26 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(9, 270);
+            this.label2.Location = new System.Drawing.Point(9, 263);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(58, 16);
             this.label2.TabIndex = 6;
             this.label2.Text = "Função";
             // 
-            // textBox1
+            // txtFuncao
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 289);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(397, 20);
-            this.textBox1.TabIndex = 7;
+            this.txtFuncao.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtFuncao.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFuncao.Location = new System.Drawing.Point(12, 282);
+            this.txtFuncao.Name = "txtFuncao";
+            this.txtFuncao.Size = new System.Drawing.Size(450, 22);
+            this.txtFuncao.TabIndex = 1;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(9, 326);
+            this.label3.Location = new System.Drawing.Point(9, 319);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(56, 16);
             this.label3.TabIndex = 8;
@@ -136,30 +142,30 @@
             this.panel1.Controls.Add(this.rbRS);
             this.panel1.Controls.Add(this.rbPR);
             this.panel1.Controls.Add(this.rbSC);
-            this.panel1.Location = new System.Drawing.Point(12, 345);
+            this.panel1.Location = new System.Drawing.Point(12, 338);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(200, 100);
-            this.panel1.TabIndex = 9;
-            // 
-            // rbSC
-            // 
-            this.rbSC.AutoSize = true;
-            this.rbSC.Location = new System.Drawing.Point(3, 39);
-            this.rbSC.Name = "rbSC";
-            this.rbSC.Size = new System.Drawing.Size(95, 17);
-            this.rbSC.TabIndex = 6;
-            this.rbSC.Text = "Santa Catarina";
-            this.rbSC.UseVisualStyleBackColor = true;
+            this.panel1.Size = new System.Drawing.Size(206, 100);
+            this.panel1.TabIndex = 2;
             // 
             // rbRS
             // 
             this.rbRS.AutoSize = true;
-            this.rbRS.Location = new System.Drawing.Point(3, 71);
+            this.rbRS.Location = new System.Drawing.Point(6, 72);
             this.rbRS.Name = "rbRS";
             this.rbRS.Size = new System.Drawing.Size(112, 17);
-            this.rbRS.TabIndex = 10;
+            this.rbRS.TabIndex = 2;
             this.rbRS.Text = "Rio Grande do Sul";
             this.rbRS.UseVisualStyleBackColor = true;
+            // 
+            // rbSC
+            // 
+            this.rbSC.AutoSize = true;
+            this.rbSC.Location = new System.Drawing.Point(6, 40);
+            this.rbSC.Name = "rbSC";
+            this.rbSC.Size = new System.Drawing.Size(95, 17);
+            this.rbSC.TabIndex = 1;
+            this.rbSC.Text = "Santa Catarina";
+            this.rbSC.UseVisualStyleBackColor = true;
             // 
             // panel2
             // 
@@ -167,91 +173,108 @@
             this.panel2.Controls.Add(this.chbCel);
             this.panel2.Controls.Add(this.chbNote);
             this.panel2.Controls.Add(this.chbChip);
-            this.panel2.Location = new System.Drawing.Point(288, 345);
+            this.panel2.Location = new System.Drawing.Point(256, 338);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(200, 100);
-            this.panel2.TabIndex = 10;
+            this.panel2.Size = new System.Drawing.Size(206, 100);
+            this.panel2.TabIndex = 3;
             // 
-            // label4
+            // chbTela
             // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(285, 326);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(106, 16);
-            this.label4.TabIndex = 11;
-            this.label4.Text = "Equipamentos";
+            this.chbTela.AutoSize = true;
+            this.chbTela.Location = new System.Drawing.Point(6, 80);
+            this.chbTela.Name = "chbTela";
+            this.chbTela.Size = new System.Drawing.Size(93, 17);
+            this.chbTela.TabIndex = 3;
+            this.chbTela.Text = "Segunda Tela";
+            this.chbTela.UseVisualStyleBackColor = true;
+            // 
+            // chbCel
+            // 
+            this.chbCel.AutoSize = true;
+            this.chbCel.Location = new System.Drawing.Point(6, 56);
+            this.chbCel.Name = "chbCel";
+            this.chbCel.Size = new System.Drawing.Size(58, 17);
+            this.chbCel.TabIndex = 2;
+            this.chbCel.Text = "Celular";
+            this.chbCel.UseVisualStyleBackColor = true;
             // 
             // chbNote
             // 
             this.chbNote.AutoSize = true;
-            this.chbNote.Location = new System.Drawing.Point(3, 8);
+            this.chbNote.Location = new System.Drawing.Point(6, 8);
             this.chbNote.Name = "chbNote";
             this.chbNote.Size = new System.Drawing.Size(73, 17);
-            this.chbNote.TabIndex = 12;
+            this.chbNote.TabIndex = 0;
             this.chbNote.Text = "Notebook";
             this.chbNote.UseVisualStyleBackColor = true;
             // 
             // chbChip
             // 
             this.chbChip.AutoSize = true;
-            this.chbChip.Location = new System.Drawing.Point(3, 32);
+            this.chbChip.Location = new System.Drawing.Point(6, 32);
             this.chbChip.Name = "chbChip";
             this.chbChip.Size = new System.Drawing.Size(47, 17);
-            this.chbChip.TabIndex = 13;
+            this.chbChip.TabIndex = 1;
             this.chbChip.Text = "Chip";
             this.chbChip.UseVisualStyleBackColor = true;
             // 
-            // chbCel
+            // label4
             // 
-            this.chbCel.AutoSize = true;
-            this.chbCel.Location = new System.Drawing.Point(3, 56);
-            this.chbCel.Name = "chbCel";
-            this.chbCel.Size = new System.Drawing.Size(58, 17);
-            this.chbCel.TabIndex = 14;
-            this.chbCel.Text = "Celular";
-            this.chbCel.UseVisualStyleBackColor = true;
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(259, 319);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(106, 16);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Equipamentos";
             // 
-            // chbTela
+            // txtMail
             // 
-            this.chbTela.AutoSize = true;
-            this.chbTela.Location = new System.Drawing.Point(3, 80);
-            this.chbTela.Name = "chbTela";
-            this.chbTela.Size = new System.Drawing.Size(93, 17);
-            this.chbTela.TabIndex = 15;
-            this.chbTela.Text = "Segunda Tela";
-            this.chbTela.UseVisualStyleBackColor = true;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(12, 483);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(397, 20);
-            this.textBox2.TabIndex = 13;
+            this.txtMail.CharacterCasing = System.Windows.Forms.CharacterCasing.Lower;
+            this.txtMail.Location = new System.Drawing.Point(12, 476);
+            this.txtMail.Name = "txtMail";
+            this.txtMail.Size = new System.Drawing.Size(450, 20);
+            this.txtMail.TabIndex = 4;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(9, 464);
+            this.label5.Location = new System.Drawing.Point(9, 457);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(51, 16);
             this.label5.TabIndex = 12;
             this.label5.Text = "E-mail";
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnEnviar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEnviar.ForeColor = System.Drawing.Color.White;
+            this.btnEnviar.Location = new System.Drawing.Point(155, 511);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(145, 38);
+            this.btnEnviar.TabIndex = 5;
+            this.btnEnviar.Text = "Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = false;
+            this.btnEnviar.Click += new System.EventHandler(this.btnEnviar_Click);
+            this.btnEnviar.MouseEnter += new System.EventHandler(this.btnEnviar_MouseEnter);
+            this.btnEnviar.MouseLeave += new System.EventHandler(this.btnEnviar_MouseLeave);
             // 
             // frmPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.ClientSize = new System.Drawing.Size(684, 661);
-            this.Controls.Add(this.textBox2);
+            this.ClientSize = new System.Drawing.Size(474, 561);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.txtMail);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtFuncao);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txtNome);
             this.Controls.Add(this.label1);
@@ -279,7 +302,7 @@
         private System.Windows.Forms.TextBox txtNome;
         private System.Windows.Forms.RadioButton rbPR;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtFuncao;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.RadioButton rbSC;
@@ -290,8 +313,9 @@
         private System.Windows.Forms.CheckBox chbChip;
         private System.Windows.Forms.CheckBox chbCel;
         private System.Windows.Forms.CheckBox chbTela;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txtMail;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnEnviar;
     }
 }
 
--- Novo_Colaborador/Novo_Colaborador/Form1.cs
+++ Novo_Colaborador/Novo_Colaborador/Form1.cs
@@ -5,7 +5,9 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -13,20 +15,96 @@ namespace Novo_Colaborador
 {
     public partial class frmPrincipal : Form
     {
-        private Color backgroundColor = Color.FromArgb(0xE7, 0xFE, 0xFB);
-
-        
         public frmPrincipal()
         {
             InitializeComponent();
         }
+        
+        private Color backgroundColor = Color.FromArgb(0xE7, 0xFE, 0xFB);
 
+        public void limpar()
+        {
+            txtNome.Clear();
+            txtFuncao.Clear();
+            txtMail.Clear();
+            chbCel.Checked = false;
+            chbChip.Checked = false;
+            chbNote.Checked = false;
+            chbTela.Checked = false;
+            rbPR.Checked = true;
+        }
 
+        static string RemoverAcentos(string mail)
+        {
+            // Remove acentos usando uma expressão regular
+            string semAcentos = Regex.Replace(mail, "[áàâãäÁÀÂÃÄ]", "a");
+            semAcentos = Regex.Replace(semAcentos, "[éèêëÉÈÊË]", "e");
+            semAcentos = Regex.Replace(semAcentos, "[íìîïÍÌÎÏ]", "i");
+            semAcentos = Regex.Replace(semAcentos, "[óòôõöÓÒÔÕÖ]", "o");
+            semAcentos = Regex.Replace(semAcentos, "[úùûüÚÙÛÜ]", "u");
+            semAcentos = Regex.Replace(semAcentos, "[çÇ]", "c");
+            return semAcentos;
+        }
 
         private void frmPrincipal_Load(object sender, EventArgs e)
         {
             this.BackColor = backgroundColor;
             lblTitulo.ForeColor = Color.FromArgb(0x08, 0x95, 0x89);
+        }
+
+
+        private void btnEnviar_MouseEnter(object sender, EventArgs e)
+        {
+            btnEnviar.BackColor = Color.White;
+            btnEnviar.ForeColor = Color.FromArgb(0x00, 0x64, 0x00);
+        }
+
+        private void btnEnviar_MouseLeave(object sender, EventArgs e)
+        {
+            btnEnviar.ForeColor = Color.White;
+            btnEnviar.BackColor = Color.FromArgb(0x00, 0x64, 0x00);
+        }
+
+        private void btnEnviar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Enviou");
+        }
+
+        private void txtNome_Leave(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtNome.Text))
+            {
+                string inputString = txtNome.Text;
+                string mail;
+
+                // Divida a string em palavras usando espaço como delimitador
+                string[] palavras = inputString.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                // Crie um array com o número de elementos correspondente ao número de palavras
+                string[] arrayDePalavras = new string[palavras.Length];
+
+                int numeroDePalavras = palavras.Length;
+
+                // Copie as palavras para o novo array
+                Array.Copy(palavras, arrayDePalavras, palavras.Length);
+
+
+                if (numeroDePalavras > 1)
+                {
+                    if (arrayDePalavras[1] == "DA" || arrayDePalavras[1] == "DE" || arrayDePalavras[1] == "DO" || arrayDePalavras[1] == "DAS" || arrayDePalavras[1] == "DOS")
+                    {
+                        mail = arrayDePalavras[0] + "." + arrayDePalavras[2];
+                    }
+                    else { mail = arrayDePalavras[0] + "." + arrayDePalavras[1]; }
+
+                }
+                else { mail = arrayDePalavras[0]; }
+
+                string resultado = RemoverAcentos(mail);
+
+                txtMail.Text = resultado + "@medicalway.com.br";
+            }
+
         }
     }
 }
